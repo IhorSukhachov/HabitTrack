e6888a4745a43fb1383d28972bf0d1e1aeed9d72
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-11-28 19:54:52 +0200

    Initial Commit

---
+++ HabitTrack/HabitTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackApp.swift
+//  HabitTrack
+//
+//  Created by Ihor Sukhachov on 28.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
