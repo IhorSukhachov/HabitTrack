af4c93742561bc0c1482fa47ff8f5ed028522725
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-11-29 12:10:53 +0200

    added on delete func

---
+++ HabitTrack/AddActivity.swift
@@ -0,0 +1,18 @@
+//
+//  AddActivity.swift
+//  HabitTrack
+//
+//  Created by Ihor Sukhachov on 29.11.2025.
+//
+
+import SwiftUI
+
+struct AddActivity: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddActivity()
+}
